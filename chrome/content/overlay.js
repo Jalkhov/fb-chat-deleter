e57a286b356ad853c66b81aa3241d8b26b6d7b8b
Pedro Torcatt <pedrotorcattsoto@gmail.com>
2022-01-15 17:49:24 -0400

    Initial commit

---
+++ chrome/content/overlay.js
@@ -0,0 +1,57 @@
+let tabs4close = [];
+
+function TabsGetter(url){
+    var tabs = [];
+    var numTabs = gBrowser.tabContainer.childNodes.length;
+    var targetUrl = url;
+
+    for (var x = 0; x < numTabs; x++) {
+        var currentTab = gBrowser.tabContainer.childNodes[x];
+
+        var tab = gBrowser.getBrowserForTab(currentTab);
+        var tabUrl = tab.contentWindow.location.href;
+        
+        if (tabUrl.includes(targetUrl)) {
+            tabs.push(tab);
+        }
+    }
+
+    return tabs;
+}
+
+function Delete(){
+    var tabs = TabsGetter("m.facebook.com/messages/read/");
+
+    for (var x = 0; x < tabs.length; x++) {
+        var tabDoc = tabs[x].contentWindow.document;
+        var deleteLink = tabDoc.getElementsByName("delete")[0];
+        deleteLink.click();
+    }
+}
+
+function confirmDelete(){
+    var tabs = TabsGetter("m.facebook.com/messagingconfirmatio");
+    tabs4close = tabs
+
+    for (var x = 0; x < tabs.length; x++) {
+        var tabDoc = tabs[x].contentWindow.document;
+        var confirmLink = tabDoc.getElementsByClassName("bk bm")[0];
+        confirmLink.click();
+
+    }
+}
+
+/*
+function closeTabs(){
+    for (var x = 0; x < tabs4close.length; x++){
+        tabs4close[x].close()
+    }
+}
+*/
+
+
+function executeEval(){
+  var code = document.getElementById('code2eval');
+  eval(code.value);
+    
+}
